e59b1e0e970b60404c931a2c57a9b6f4228c685d
ashismo <ashishkrmondal@gmail.com>
2019-02-02 21:13:04 -0500

    updated build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ login_process=`ps -ef | grep login-jwt | awk '$8=="java" {print $2}'`
 echo $login_process
 kill -9 $login_process
 echo "Running application has been stopped. Starting the application again"
-mvn -DskipTests=true clean install
-java -jar target/cactusschool-login-jwt-1.0.jar &
+mvn clean install
+#java -jar target/cactusschool-login-jwt-1.0.jar &
